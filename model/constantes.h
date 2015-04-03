732773986f9bd5b97be2aa17328d64cfad34bd7d
Federico Seckel <fseckel@dc.uba.ar>
2015-04-03 18:44:40 -0300

    agregado archivo de constantes

---
+++ model/constantes.h
@@ -0,0 +1,23 @@
+#ifndef CONSTANTES_H_
+#define CONSTANTES_H_
+
+
+const int ANCHOVENTANAPX = 800;
+const int ALTOVENTANAPX = 600;
+const float ANCHOVENTANAL = 200;
+
+
+
+const float ANCHOESCENARIO = 1000.5;
+const float ALTOESCENARIO = 150;
+const float ALTURAPISOESCENARIO = 20;
+
+const float ANCHOCAPA = 500;
+
+const float ANCHOPERSONAJE = 20;
+const float ALTOPERSONAJE = 35;
+const float ZINDEXPERSONAJE = 1
+const float SPRITESPERSONAJE = 8;
+
+
+#endif /* CONSTANTES_H_ */
