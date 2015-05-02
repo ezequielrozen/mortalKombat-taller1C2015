d1ae3ca6a3a850fe85350851db8c1509b37cce23
gmerino92 <merino.gonza@gmail.com>
2015-05-02 02:34:41 -0300

    Fix bug Collider

--- controller/Collider.cpp
+++ controller/Collider.cpp
@@ -10,8 +10,10 @@ Collider::~Collider() {
 }
 
 void Collider::update(MKCharacter* character1, MKCharacter* character2, bool cameraMoved) {
-	if ((!((character1->getX()+character1->getWidth() == character2->getX() && character1->getMovement() == "RIGHT") || (character1->getX() == character2->getX()+character2->getWidth() && character1->getMovement() == "LEFT"))) && !(cameraMoved))
+
+	if ((!((character1->getX()+character1->getWidth() >= character2->getX() && character1->getMovement() == "RIGHT" && character1->getX() < character2->getX()) || (character1->getX() <= character2->getX()+character2->getWidth() && character1->getMovement() == "LEFT" && character1->getX() > character2->getX()))) && !(cameraMoved))
 	{
 		character1->Update();
 	}		
+
 }
