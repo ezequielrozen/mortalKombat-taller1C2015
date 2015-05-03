876c7313b9707b4b521bb94ba1cb63ce56a94396
HernanAA <hernandres64@hotmail.com>
2015-05-03 16:07:44 -0300

    cambie varios if else por un switch

--- controller/GameController.cpp
+++ controller/GameController.cpp
@@ -27,63 +27,58 @@ void GameController::update(MKCharacter* character, MKCharacter* character2) {
 
     if (mainEvent->type == SDL_KEYDOWN)
     {
-        if (mainEvent->key.keysym.sym == SDLK_RIGHT)
-        {
-            Mylog->Log("movimiento del personaje: hacia la derecha", ERROR_LEVEL_INFO);
-            character->setMovement("RIGHT");
-            timer = SDL_GetTicks();
-            //cout << "right" << endl;
-        } else if (mainEvent->key.keysym.sym == SDLK_d)
-        {
-            Mylog->Log("movimiento del personaje: hacia la derecha", ERROR_LEVEL_INFO);
-            character2->setMovement("RIGHT");
-            timer = SDL_GetTicks();
-            //cout << "right" << endl;
-        }
-        else if (mainEvent->key.keysym.sym == SDLK_LEFT)
-        {
-            Mylog->Log("movimiento del personaje: hacia la izquierda", ERROR_LEVEL_INFO);
-            character->setMovement("LEFT");
-            timer = SDL_GetTicks();
-
-        } else if (mainEvent->key.keysym.sym == SDLK_a)
-        {
-            Mylog->Log("movimiento del personaje: hacia la izquierda", ERROR_LEVEL_INFO);
-            character2->setMovement("LEFT");
-            timer = SDL_GetTicks();
-
-        }
-        else if (mainEvent->key.keysym.sym == SDLK_UP)
-		{
-        	Mylog->Log("movimiento del personaje: hacia arriba", ERROR_LEVEL_INFO);
-            character->setJump(true);
-		} else if (mainEvent->key.keysym.sym == SDLK_w)
-        {
-            Mylog->Log("movimiento del personaje: hacia arriba", ERROR_LEVEL_INFO);
-            character2->setJump(true);
-        }
-        else if (mainEvent->key.keysym.sym == SDLK_DOWN)
-		{
-        	Mylog->Log("movimiento del personaje: agachandose", ERROR_LEVEL_INFO);
-            character->setMovement("DUCK");
-            timer = SDL_GetTicks();
-		} else if (mainEvent->key.keysym.sym == SDLK_s)
-        {
-            Mylog->Log("movimiento del personaje: agachandose", ERROR_LEVEL_INFO);
-            character2->setMovement("DUCK");
-            timer = SDL_GetTicks();
-        }
-        else if (mainEvent->key.keysym.sym == SDLK_k)
-		{
-        	Mylog->Log("movimiento del personaje: Pateando", ERROR_LEVEL_INFO);
-            character->setMovement("KICK");
-            timer = SDL_GetTicks();
-		} else if (mainEvent->key.keysym.sym == SDLK_f)
-        {
-            Mylog->Log("movimiento del personaje: Pateando", ERROR_LEVEL_INFO);
-            character2->setMovement("KICK");
-            timer = SDL_GetTicks();
-        }
+    	switch(mainEvent->key.keysym.sym){
+			case SDLK_RIGHT:
+				Mylog->Log("movimiento del personaje: hacia la derecha", ERROR_LEVEL_INFO);
+				character->setMovement("RIGHT");
+				timer = SDL_GetTicks();
+				break;
+			case SDLK_d:
+				Mylog->Log("movimiento del personaje: hacia la derecha", ERROR_LEVEL_INFO);
+				character2->setMovement("RIGHT");
+				timer = SDL_GetTicks();
+				break;
+			case SDLK_LEFT:
+				Mylog->Log("movimiento del personaje: hacia la izquierda", ERROR_LEVEL_INFO);
+				character->setMovement("LEFT");
+				timer = SDL_GetTicks();
+				break;
+			case SDLK_a:
+				Mylog->Log("movimiento del personaje: hacia la izquierda", ERROR_LEVEL_INFO);
+				character2->setMovement("LEFT");
+				timer = SDL_GetTicks();
+				break;
+			case SDLK_UP:
+				Mylog->Log("movimiento del personaje: hacia arriba", ERROR_LEVEL_INFO);
+				character->setJump(true);
+				break;
+			case SDLK_w:
+				Mylog->Log("movimiento del personaje: hacia arriba", ERROR_LEVEL_INFO);
+				character2->setJump(true);
+				break;
+			case SDLK_DOWN:
+				Mylog->Log("movimiento del personaje: agachandose", ERROR_LEVEL_INFO);
+				character->setMovement("DUCK");
+				timer = SDL_GetTicks();
+				break;
+			case SDLK_s:
+				Mylog->Log("movimiento del personaje: agachandose", ERROR_LEVEL_INFO);
+				character2->setMovement("DUCK");
+				timer = SDL_GetTicks();
+				break;
+			case SDLK_k:
+				Mylog->Log("movimiento del personaje: Pateando", ERROR_LEVEL_INFO);
+				character->setMovement("KICK");
+				timer = SDL_GetTicks();
+				break;
+			case SDLK_f:
+				Mylog->Log("movimiento del personaje: Pateando", ERROR_LEVEL_INFO);
+				character2->setMovement("KICK");
+				timer = SDL_GetTicks();
+				break;
+			default:
+				break;
+		}
     }
     else if (timer + COMMANDDELAY < SDL_GetTicks()) {
 
