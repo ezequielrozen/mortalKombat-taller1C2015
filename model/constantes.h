849256355764edf19913231140e1c3821dbc8d49
ezequiel <ezequielrozen@gmail.com>
2015-06-07 16:07:32 -0300

    Unnecessary onst deleted

--- model/constantes.h
+++ model/constantes.h
@@ -15,7 +15,6 @@ const float ANCHOESCENARIO                  = 1000;
 const float ALTOESCENARIO                   =  400;
 const float ALTURAPISOESCENARIO             =   20;
 const float LIFEBAR_RELATIVE_WIDTH          = 0.4; // Relative to logical window size
-const double TIMER_RELATIVE_WIDTH			= 0.2*0.9;
 const float VIBRATION_SPEED                 = 0.8;
 const int   VIBRATION_DURATION              = 33;
 //
